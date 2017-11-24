ae9f535b2976da15fcda95261839db7c1da13f04
Ny'alotha <Nyalotha@outlook.com>
2017-11-24 14:32:34 +0200

    InsertionSort

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -10,6 +10,19 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[] { 5, 2, 4, 6, 1, 3 };
+            StringBuilder sortedArray = new StringBuilder();
+
+            SortingAlgorithms.InsertionSort(ref arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sortedArray.Append(arr[i] + " ");
+            }
+
+            Console.WriteLine(sortedArray);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ CSharp/SortingAlgorithms.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class SortingAlgorithms
+    {
+        // Swaps two values in an array
+        public static void Exchange(ref int[] arr, int m, int n)
+        {
+            int temp = arr[m];
+
+            arr[m] = arr[n];
+            arr[n] = temp;
+        }
+
+        // Insertion sort
+        public static void InsertionSort(ref int[] arr)
+        {
+            int temp, j;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                temp = arr[i];
+                j = i - 1;
+                while (j >= 0 && arr[j] > temp)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                arr[j + 1] = temp;
+            }
+        }
+
+    }
+}
